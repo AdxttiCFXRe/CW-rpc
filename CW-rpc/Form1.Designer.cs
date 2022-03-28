eaff1647d4c91e96a01505d7fc8a3fecdad28c3b
AdxttiCFXRe <102541864+AdxttiCFXRe@users.noreply.github.com>
2022-03-28 12:23:13 -0400

    CW-rpc
    CW-rpc C# WPF source code. Credits to AdxttiCFXRe. https://forum.cfx.re/u/adxtticfxre/

---
+++ CW-rpc/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace CW_rpc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.codeWalkerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.joinDiscordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.launchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.documentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic CodeWalker Rich Presence using msciotti\'s Discord-RPC\r\nCredits to dexyfex f" +
+    "or CW";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "by AdxttiCFXRe";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.codeWalkerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(393, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // codeWalkerToolStripMenuItem
+            // 
+            this.codeWalkerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.launchToolStripMenuItem,
+            this.joinDiscordToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.quitToolStripMenuItem,
+            this.documentationToolStripMenuItem});
+            this.codeWalkerToolStripMenuItem.Name = "codeWalkerToolStripMenuItem";
+            this.codeWalkerToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.codeWalkerToolStripMenuItem.Text = "CodeWalker";
+            // 
+            // joinDiscordToolStripMenuItem
+            // 
+            this.joinDiscordToolStripMenuItem.Name = "joinDiscordToolStripMenuItem";
+            this.joinDiscordToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.joinDiscordToolStripMenuItem.Text = "Join Discord";
+            this.joinDiscordToolStripMenuItem.Click += new System.EventHandler(this.joinDiscordToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Quit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // launchToolStripMenuItem
+            // 
+            this.launchToolStripMenuItem.Name = "launchToolStripMenuItem";
+            this.launchToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.launchToolStripMenuItem.Text = "Launch";
+            this.launchToolStripMenuItem.Click += new System.EventHandler(this.launchToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // documentationToolStripMenuItem
+            // 
+            this.documentationToolStripMenuItem.Name = "documentationToolStripMenuItem";
+            this.documentationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.documentationToolStripMenuItem.Text = "GitHub";
+            this.documentationToolStripMenuItem.Click += new System.EventHandler(this.documentationToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 173);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CW-rpc by AdxttiCFXRe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem codeWalkerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem joinDiscordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem launchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem documentationToolStripMenuItem;
+    }
+}
+
---
+++ CW-rpc/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRpcDemo;
+
+
+namespace CW_rpc
+{
+
+    public partial class Form1 : Form
+    {
+
+        private DiscordRpc.EventHandlers handlers;
+        private DiscordRpc.RichPresence presence;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async Task DetectCWProcess()
+        {
+            for (;;)
+            {
+                Process[] processes = Process.GetProcessesByName("CodeWalker");
+                if (processes.Length == 0)
+                {
+                    MessageBox.Show("CodeWalker is not running! Launch it to use CW-rpc without any issues.", "CW-rpc", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+                else
+                {
+                    //to-do: Elapsed Time
+                    //Stopwatch stopWatch = new Stopwatch();
+                    //stopWatch.Start();
+                    this.handlers = default(DiscordRpc.EventHandlers);
+                    DiscordRpc.Initialize("958026542959697981", ref this.handlers, true, null);
+                    this.handlers = default(DiscordRpc.EventHandlers);
+                    DiscordRpc.Initialize("958026542959697981", ref this.handlers, true, null);
+                    //var ts = stopWatch.Elapsed;
+                    //string elapsedTime = $"{ts.Hours}:{ts.Minutes}:{ts.Seconds}.{ts.Milliseconds / 10}";
+                    this.presence.details = "CodeWalker by dexyfex";
+                    //this.presence.state = "using CW-rpc by AdxttiCFXRe";
+                    this.presence.largeImageKey = "cw4";
+                    //this.presence.smallImageKey = "image";
+                    DiscordRpc.UpdatePresence(ref this.presence);
+                }
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await Task.Run(DetectCWProcess);   
+        }
+
+        private void joinDiscordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/37hFAydgAY");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CodeWalker/CW is a GTA V 3D Map Editor created by dexyfex.");
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CW-rpc is a CodeWalker Discord Rich Presence using msciotti's discord-rpc.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void launchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(@"CodeWalker.exe");
+            }
+            catch
+            {
+                MessageBox.Show("Please place CW-rpc in your CodeWalker directory!", "CW-rpc", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("CodeWalker");
+            if (processes.Length == 0)
+            {
+                MessageBox.Show("CodeWalker is not running!", "CW-rpc", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                processes[0].CloseMainWindow();
+            }
+        }
+
+        private void documentationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/dexyfex/CodeWalker");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
